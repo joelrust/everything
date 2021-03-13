5d468a80a5c6375d6dc941157437470f46092533
joelrust <35672157+joelrust@users.noreply.github.com>
2021-03-13 11:24:48 -0500

    can't remember... stuff

--- js/src4.js
+++ js/src4.js
@@ -23,16 +23,16 @@ var isFirstClick = true;
 
 const demoSets = [
 //	[pitch, word, length, dynamic, countdown, tutorial]
- 	["60", "&nbsp", "&nbsp;", 0, 5, "You’ll see a countdown on your own screen. When this finishes, you will hear a note. If the ping was too quiet or too loud, you can adjust the volume using the slider at the bottom. Then, press NEXT." ],
-	["60", "&nbsp", "&nbsp;", 0, 5, "You’ll see a countdown on your own screen. When this finishes, you will hear a note. If the ping was too quiet or too loud, you can adjust the volume using the slider at the bottom. Then, press NEXT." ],
-	["61", "sun", "&nbsp;", 0, 4, "This time, you will also see a word — sing the word on the note you hear. Then, press NEXT." ],
-	["60", "fall", "—", 0, 3, "Now, there will be a — symbol above the word. This means to sing it slowly — make it last 4 to 6 seconds. When you’ve finished, press NEXT." ],
-	["60", "ocean", "◡", 0, 3, "This word will have a ◡ symbol above it. This means to sing it at a normal spoken pace — so it will be a lot shorter than the last one. When it’s over, press NEXT." ],
-	["61", "green", "—", 4, 3, "One more thing. In the black band underneath the word, you’ll be given an indication of how quiet or loud to sing each word. By default, these use the dynamic markings used in sheet music.  But, if you’re unfamiliar with that system, you can switch it to a different one at the bottom, which indicates the loudness with the height of a line, |⎽| being quietest and |⎺| being loudest. Press NEXT." ],
-	["59", "hidden", "◡", 1, 3, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than other singers, or if there are some notes you don’t sing. All participants will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
-	["60", "oyster", "◡", 3, 4, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than other singers, or if there are some notes you don’t sing. All participants will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
-	["61", "treadmill", "—", 4, 5, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than other singers, or if there are some notes you don’t sing. All participants will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
-	["60", "spanner", "◡", 2, 3, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than other singers, or if there are some notes you don’t sing. All participants will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
+ 	["60", "&nbsp", "&nbsp;", 0, 5, "<br>You’ll see a countdown on your own screen. When this finishes, you will hear a note. If the ping was too quiet or too loud, you can adjust the volume using the slider at the bottom. Then, press NEXT." ],
+	["60", "&nbsp", "&nbsp;", 0, 5, "<br>You’ll see a countdown on your own screen. When this finishes, you will hear a note. If the ping was too quiet or too loud, you can adjust the volume using the slider at the bottom. Then, press NEXT." ],
+	["61", "sun", "&nbsp;", 0, 4, "<br><br>This time, you will also see a word — sing the word on the note you hear. Then, press NEXT." ],
+	["60", "fall", "—", 0, 3, "<br><br>Now, there will be a &quot;—&quot; symbol above the word. This means to sing it slowly; make it last 4 to 6 seconds. When you’ve finished, press NEXT." ],
+	["60", "ocean", "◡", 0, 3, "<br><br>This word will have a &quot;◡&quot; symbol above it. This means to sing it at a normal spoken pace; so it will be a lot shorter than the last one. When it’s over, press NEXT." ],
+	["61", "green", "—", 4, 3, "<br>One more thing. In the black band underneath the word, you’ll be given an indication of how quiet or loud to sing each word. Below, there's a toggle where you can switch between symbols or words. Press NEXT." ],
+	["59", "hidden", "◡", 1, 3, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than others, or if there are notes you don’t sing. Everyone will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
+	["60", "oyster", "◡", 3, 4, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than others, or if there are notes you don’t sing. Everyone will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
+	["61", "treadmill", "—", 4, 5, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than others, or if there are notes you don’t sing. Everyone will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
+	["60", "spanner", "◡", 2, 3, "Each singer has their own part, which is randomly generated; it’s OK if you’re moving faster or slower than others, or if there are some you don’t sing. Everyone will, at the same time, get the message [stop]. If you’re singing, finish the word you’re on. Then, the piece is over. Until then, practice with a few more words!" ],
 	
 	
 ]
@@ -41,7 +41,7 @@ const demoSets = [
 
 var dynamicSet = ["&nbsp;", "<i>p</i>", "<i>mp</i>", "<i>mf</i>", "<i>f</i>", "<i>ff</i>"];
 
-const dynamicSymbols = ["&nbsp;", "|⎼|", "|–|", "|–|", "|⎻|", "|⎺|"];
+const dynamicSymbols = ["&nbsp;", "quiet", "medium", "medium", "loud", "loud"];
 
 const dynamicLetters = ["&nbsp;", "<i>p</i>", "<i>mp</i>", "<i>mf</i>", "<i>f</i>", "<i>ff</i>"];
 
